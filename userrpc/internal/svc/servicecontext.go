79c02111a1f087f124a3208248b9ca4cd4a3f373
korman <korman@qq.com>
2022-12-06 21:25:20 +0800

    addes usrrpc

---
+++ userrpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "wow_server/userrpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
